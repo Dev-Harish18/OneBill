f3c2e5b7353460acf63d4893b3e2aae350e07c56
Harish V <harishvilar91@gmail.com>
2021-07-11 15:20:53 +0530

    Update ProtectedRoute.js

--- One Bill/src/components/ProtectedRoute.js
+++ One Bill/src/components/ProtectedRoute.js
@@ -6,34 +6,24 @@ import GetProducts from "./GetProducts";
 const ProtectedRoute = ({ component: Component, user, ...rest }) => {
   const location = useLocation();
   console.log(location.user);
-  // const [path, setPath] = useState("");
+  
   let path = "";
   path = location.pathname;
   localStorage.setItem("path", path);
-  // // useEffect(() => {
-  // //   setPath(location.pathname);
-  // // }, [])
+
   return (
     <Route
       {...rest}
       render={(props) => {
         if (user) {
-          // path = location.pathname;
-          // console.log(`Current Path ${path}`);
           return <Component {...rest} {...props} />;
         } else {
           alert("Please Login first");
-          // setPath(ppath);
-          // console.log(`Previous Path ${path}`)
+          
           return (
             <Redirect
               to="/"
-              // to={{
-              //   pathname: "/",
-              //   state: {
-              //     from: props.location,
-              //   },
-              // }}
+              
             />
           );
         }
